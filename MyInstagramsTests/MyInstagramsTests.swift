a69b8a4a376502572932401133b32a32fd225795
piyaphorn schueller <550107030043@dpu.ac.th>
2016-03-24 12:56:19 +0700

    Myinstagrams stater project with cocoapods setup

---
+++ MyInstagramsTests/MyInstagramsTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyInstagramsTests.swift
+//  MyInstagramsTests
+//
+//  Created by DE DPU on 3/24/2559 BE.
+//  Copyright © 2559 DE DPU. All rights reserved.
+//
+
+import XCTest
+@testable import MyInstagrams
+
+class MyInstagramsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
