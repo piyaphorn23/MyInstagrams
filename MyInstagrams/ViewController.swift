3b942d539e35ae3e53add55bec7d61c78169d376
piyaphorn schueller <550107030043@dpu.ac.th>
2016-03-25 12:38:02 +0700

    Added kumulosAPI and tested with createUser

--- MyInstagrams/ViewController.swift
+++ MyInstagrams/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uname: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var repass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnsignup() {
+        if pass.text == repass.text{
+            let kumu = Kumulos()
+            kumu.createUserWithUsername(uname.text, andPassword: pass.text, andEmail: email.text, andAuthData: "Test")
+        }
+    }
 
+    @IBAction func btncancel() {
+        uname.text = ""
+        email.text = ""
+        pass.text = ""
+        repass.text = ""
+    }
 }
 
